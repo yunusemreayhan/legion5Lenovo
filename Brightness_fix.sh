58d441bb3c36a98a020fde183418bcadd330c93a
Yunus Emre AYHAN <ayhanyunusemre@gmail.com>
2026-03-29 14:47:26 +0300

    Remove duplicated scripts, keep Ubuntu-specific fixes

--- Brightness_fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#This line fixes the brightness control issue while using AMD Gpu
-sudo grubby --args="amdgpu.backlight=0" --update-kernel $(sudo grubby --default-kernel)
--- Compiz_fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#This line fixes the issues while using compiz with AMD Gpu
-sudo echo "export AMD_DEBUG=nodcc" > /etc/profile.d/amd_fix.sh
